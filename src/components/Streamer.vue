0f53569946b4cfcadfbdff044b374b819b0150e1
Nyaron <cedricd88+gth@gmail.com>
2018-09-11 16:13:49 +0200

    :sparkles: Implement Firestore
    Replace hard-coded streamers list with calls to cloud firestore and add some checks and improvement to the single streamer section.

--- src/components/Streamer.vue
+++ src/components/Streamer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="streamer">
     <streamer-nav></streamer-nav>
-    <router-view></router-view>
+    <router-view v-if="isOG"></router-view>
   </div>
 </template>
 
@@ -11,5 +11,17 @@ import StreamerNav from './StreamerNav';
 export default {
   name: 'Streamer',
   components: { StreamerNav },
+  computed: {
+    isOG() {
+      const self = this;
+      if (self.$streamers) {
+        if (typeof self.$streamers.find(streamer => streamer.login === self.$route.params.channel) === 'undefined') {
+          self.$router.push({ path: '/' });
+        }
+        return true;
+      }
+      return false;
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLinkSquareAlt, faStar, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Vue from 'vue';
+import VueFire from 'vuefire';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import App from './App';
 import router from './router';
 
+Vue.use(VueFire);
+firebase.initializeApp({
+  projectId: process.env.PROJECT_ID,
+  databaseURL: process.env.DATABASE_URL,
+});
+
+const db = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true,
+};
+db.settings(settings);
+export { db as default };
+
 window.eventBus = new Vue();
 
-library.add(fab, faExternalLinkSquareAlt);
+library.add(fab, faExternalLinkSquareAlt, faStar, faCheckCircle);
 
 Vue.config.productionTip = false;
 
